f90f68e277ae928a20fd5db05fd180f2a92e84ed
Amit-Tsabary <53346107+Amit-Tsabary@users.noreply.github.com>
2022-06-05 04:29:11 +0300

    Delete main directory

--- main/java/com/example/amitproject11/ButtonDisplay.java
+++
@@ -1,9 +0,0 @@
-package com.example.amitproject11;
-
-import android.widget.Button;
-
-import java.util.ArrayList;
-
-public interface ButtonDisplay {
-    public Button createButton(String params);
-}
